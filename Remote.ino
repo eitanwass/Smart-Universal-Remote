0c024919b52695b002f9f961e8cb19f6ca915b05
eitanwass <eitanwass@gmail.com>
2018-09-15 18:09:16 +0300

    Initial upload of first working version
    First working version.
    Only records one signal and only has one button

---
+++ Remote.ino
@@ -0,0 +1,58 @@
+/* Universal Smart Remote
+ * 
+ * USR built with Arduino 1.8.5.
+ * 
+ * Ethan Wass
+ */
+
+#include <IRremote.h>
+
+
+const int RECV_PIN = 4;
+const int BTN_PIN = 5;
+
+IRrecv irrecv(RECV_PIN);
+IRsend irsend;
+decode_results results;
+
+bool BTN_pressed = false;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(BTN_PIN, INPUT);
+  
+  irrecv.enableIRIn();    //Enable recieving signals.
+}
+
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
+      //If recieved a signal decode it
+      
+      Serial.println(results.value, HEX);
+      irrecv.resume();    //Resume recieving signals.
+  }
+
+  if(digitalRead(BTN_PIN))
+  {
+    //If button pressed
+    
+    if(!BTN_pressed)
+      {
+        irsend.sendNEC(results.value, results.bits);
+        
+        Serial.print("Sent: ");
+        Serial.println(results.value, HEX);
+        
+        BTN_pressed = true;
+      }
+    irrecv.enableIRIn();    //re-enable recieveing signals
+  }
+  else
+    BTN_pressed = false;
+}
+
